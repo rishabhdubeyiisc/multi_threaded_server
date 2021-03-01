af74416bd565afa7f21385fe4d24783629323c27
rishabhdubeyiisc <rishidd7devx2@gmail.com>
2021-03-01 18:57:27 +0530

    loop added

--- udp_simple_client.py
+++ udp_simple_client.py
@@ -10,10 +10,12 @@ client_sock = socket.socket( family = socket.AF_INET, type= socket.SOCK_DGRAM )
 
 payload = "Hello from client 1902388474 1.098377328329 1.0011 0x128900238 "
 
-client_sock.sendto( payload.encode('utf-8'),( SERVER_IP , SERVER_PORT) )
-
-data_recvd , server_addr = client_sock.recvfrom(BUFFER_SIZE)
-
-print ("Server says" + str (data_recvd.decode('utf-8')))
-
+while True:
+    #send
+    client_sock.sendto( payload.encode('utf-8'),( SERVER_IP , SERVER_PORT) )
+    #recieve
+    data_recvd , server_addr = client_sock.recvfrom(BUFFER_SIZE)
+    print ("Server says" + str (data_recvd.decode('utf-8')))
+    #take input
+    new_payload = input("insert new payload > ")
 client_sock.close()
