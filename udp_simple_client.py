9cf25ca961d13f48fd1bcd79775242cfb601ea3b
rishabhdubeyiisc <rishidd7devx2@gmail.com>
2021-03-01 17:12:43 +0530

    floating point payload

--- udp_simple_client.py
+++ udp_simple_client.py
@@ -1,15 +1,16 @@
 #! /usr/bin/env python3
 import socket
 
-client_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-message_to_send = "Hello from client"
 
 SERVER_IP = '10.64.37.35'
 SERVER_PORT = 12345
 BUFFER_SIZE = 1024
 
-client_sock.sendto(message_to_send.encode('utf-8'),( SERVER_IP , SERVER_PORT) )
+client_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+payload = "Hello from client 1902388474 1.098377328329 1.0011 0x128900238 "
+
+client_sock.sendto( payload.encode('utf-8'),( SERVER_IP , SERVER_PORT) )
 
 data_recvd , server_addr = client_sock.recvfrom(BUFFER_SIZE)
 
